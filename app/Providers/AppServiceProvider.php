bac46ceac015152d0da45a1cabdfd26bfad532dd
Alberto Sanchez <albertosanchezc98@gmail.com>
2026-04-28 13:14:28 -0600

    Fix: lang en to es

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\App;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,5 +22,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        App::setLocale('es');
+        Carbon::setLocale('es');
     }
 }
